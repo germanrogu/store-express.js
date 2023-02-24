987a1029f2b68a4143441d9119a4d53248ae1375
germanrogu <german.rodriguezgu@gmail.com>
2023-02-23 22:43:47 -0500

    Routing with express

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+app.get('/other', (req, res) => {
+  res.send('Other route');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
 });
