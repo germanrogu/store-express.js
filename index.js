33f275d4c534c2f0f7136502ae608b044c18885c
germanrogu <german.rodriguezgu@gmail.com>
2023-02-25 20:53:00 -0500

    Create post service

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+//Express Middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
