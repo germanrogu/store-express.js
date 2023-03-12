c2137532c6a7b38b54f50c36704f5deb0bd9826b
germanrogu <german.rodriguezgu@gmail.com>
2023-03-11 21:42:58 -0500

    Create service to handle function endpoint - use async functions

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,38 +1,58 @@
 const express = require('express');
-const faker = require('faker');
+const ProductsService = require('../services/productsService');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const { size } = req.query;
-  const limit = Number(size) || 10;
+//Crear una instancia
+const service = new ProductsService();
 
-  const productCreator = () => ({
-    name: faker.commerce.productName(),
-    price: Number(faker.commerce.price()),
-    avatar: faker.image.imageUrl(),
-  });
-  const products = new Array(limit).fill(null).map(() => productCreator());
-  res.json(products);
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await service.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/filter', (req, res) => {
+router.get('/filter', async (req, res) => {
   res.send('Filter');
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    name: 'Producto 1',
-    price: 2000,
-    id,
-  });
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    if (product) {
+      res.status(201).json(product);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
+router.post('/', async (req, res) => {
+  const newProduct = await service.create(req.body);
+  res.status(201).json(newProduct);
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await service.delete(id);
   res.json({
-    message: 'Created',
-    data: body,
+    message: 'Deleted',
+    product,
   });
 });
 
